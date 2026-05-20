48a54643a9ab38617e2ca93db6da49d52629661c
Zephyr Lykos <git@mochaa.ws>
2026-05-21 01:09:49 +0800

    feat: also wrap parseExpression

--- packages/parser/mod.ts
+++ packages/parser/mod.ts
@@ -1,6 +1,5 @@
-import type { ParserOptions } from "@babel/parser";
-import type { File } from "@babel/types";
-import { parse as babelParse } from "@babel/parser";
+import type * as t from "@babel/types";
+import * as babel from "@babel/parser";
 import { buildOptions } from "./lib/options.ts";
 
 export {
@@ -13,6 +12,17 @@ export {
  * Wraps `parse` from `@babel/parser`, but sets default options such that as few
  * restrictions as possible are placed on the `input` code.
  */
-export function parse(input: string, options?: ParserOptions): File {
-  return babelParse(input, buildOptions(options));
+export function parse(input: string, options?: babel.ParserOptions): t.File {
+  return babel.parse(input, buildOptions(options));
+}
+
+/**
+ * Wraps `parseExpression` from `@babel/parser`, but sets default options such that as few
+ * restrictions as possible are placed on the `input` code.
+ */
+export function parseExpression(
+  input: string,
+  options?: babel.ParserOptions,
+): babel.ParseResult<t.Expression> {
+  return babel.parseExpression(input, buildOptions(options));
 }
